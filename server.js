bc6391bf4c597c019d802306f3f5d47ab1d65805
umaiibo <j.hnpn123@gmail.com>
2020-04-19 13:24:16 -0700

    added required code used for express and body-parser

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
+
+var express = require('express')
+var bodyParser = require('body-parser')
+ 
+var app = express()
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
